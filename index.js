4b4b34b31e4cd763f9fcd53659f66b17ab113023
Sanjivani1318 <jadhavsanjivani480@gmail.com>
2025-09-05 23:33:58 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+// require("dotenv").config();
+// const cors = require("cors");
+// const aiRoutes = require("./src/routes/ai.routes"); // ✅ Correct path
+
+const app = express();
+const PORT = 3002;
+// app.use(cors()); // ✅ Allow all origins for now
+// ✅ Middleware to parse JSON
+// app.use(express.json());
+
+// app.use('/ai', aiRoutes); // ✅ Use the router
+
+app.listen(PORT, () => {
+  console.log("server started");
+});
